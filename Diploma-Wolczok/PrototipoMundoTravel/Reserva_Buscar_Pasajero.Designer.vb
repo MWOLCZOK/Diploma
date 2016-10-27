5df294196975eba1bb03a6b917de71837c5b38e9
SantaT89 <santa.tassara@gmail.com>
2016-10-26 23:11:12 -0300

    un capo

---
+++ Diploma-Wolczok/PrototipoMundoTravel/Reserva_Buscar_Pasajero.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Reserva_Buscar_Pasajero
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(184, 37)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(114, 13)
+        Me.Label9.TabIndex = 35
+        Me.Label9.Text = "Busqueda de Pasajero"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(31, 62)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(435, 20)
+        Me.TextBox9.TabIndex = 34
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(99, 457)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 39)
+        Me.Button1.TabIndex = 45
+        Me.Button1.Text = "Cancelar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(293, 457)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 39)
+        Me.Button2.TabIndex = 44
+        Me.Button2.Text = "Seleccionar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(31, 116)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView2.Size = New System.Drawing.Size(435, 318)
+        Me.DataGridView2.TabIndex = 43
+        '
+        'Reserva_Buscar_Pasajero
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(512, 508)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Name = "Reserva_Buscar_Pasajero"
+        Me.Text = "DetalleReserva"
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+End Class
---
+++ Diploma-Wolczok/PrototipoMundoTravel/Reserva_Buscar_Pasajero.vb
@@ -0,0 +1,62 @@
+﻿Imports EE
+Imports BLL
+
+Public Class Reserva_Buscar_Pasajero
+
+    Protected Friend reservaAlojamiento As EE.BE_ReservaAlojamiento
+    Protected Friend reservaDestino As EE.BE_Destino
+    Protected Friend Alojamiento As EE.BE_Alojamiento
+
+
+    Sub New(ByVal paramAlojamiento As BE_Alojamiento, ByVal paramDestino As BE_Destino, ByVal paramReservaAlojamiento As BE_ReservaAlojamiento)
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+        iniciar(paramAlojamiento, paramDestino, paramReservaAlojamiento)
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+
+    End Sub
+    Private Sub iniciar(ByVal paramAlojamiento As BE_Alojamiento, ByVal paramDestino As BE_Destino, ByVal paramReservaAlojamiento As BE_ReservaAlojamiento)
+        reservaAlojamiento = paramReservaAlojamiento
+        reservaDestino = paramDestino
+        Alojamiento = paramAlojamiento
+    End Sub
+
+
+    Private Sub TextBox9_TextChanged(sender As Object, e As EventArgs) Handles TextBox9.TextChanged
+        Try
+            If TextBox9.Text.Length >= 3 Then
+                Dim oPasajero As New EE.BE_Pasajero
+                oPasajero.DNI = Me.TextBox9.Text
+                Dim _bllPasajero As New BLL.BLL_Pasajero
+                Dim listaPAsajeros As New List(Of EE.BE_Pasajero)
+                listaPAsajeros = _bllPasajero.consultarPasajeroporDNI(oPasajero)
+                Me.DataGridView2.DataSource = Nothing
+                Me.DataGridView2.DataSource = listaPAsajeros
+                DataGridView2.ReadOnly = True
+            End If
+        Catch ex As Exception
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim oPasajero As New EE.BE_Pasajero
+        oPasajero.ID = CInt(Me.DataGridView2.SelectedRows.Item(0).Cells(0).Value)
+        Dim bblPäsajero As New BLL.BLL_Pasajero
+        oPasajero = bblPäsajero.consultarPasajero(oPasajero)
+        reservaAlojamiento.Pasajero = oPasajero
+        Dim bllReserva As New BLL_Reserva
+        bllReserva.altaReserva(reservaAlojamiento)
+        Dim formularioSeleccionHabitacion As New Reserva_Detalle(Alojamiento, reservaDestino, reservaAlojamiento)
+        formularioSeleccionHabitacion.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Reserva_Buscar_Pasajero_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
