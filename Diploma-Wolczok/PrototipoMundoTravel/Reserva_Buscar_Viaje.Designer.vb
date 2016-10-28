5bb8f6ddfb9b83afb49acaa109264a368d16e3ec
mwolczok <matias.wolczok@gmail.com>
2016-10-28 02:39:41 -0300

    Reserva Viaje

---
+++ Diploma-Wolczok/PrototipoMundoTravel/Reserva_Buscar_Viaje.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Reserva_Buscar_Viaje
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btncancelar = New System.Windows.Forms.Button()
+        Me.Btnseleccionar = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Btnbuscar = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Cbxorigen = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Cbxdestino = New System.Windows.Forms.ComboBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Btncancelar
+        '
+        Me.Btncancelar.Location = New System.Drawing.Point(219, 489)
+        Me.Btncancelar.Name = "Btncancelar"
+        Me.Btncancelar.Size = New System.Drawing.Size(100, 39)
+        Me.Btncancelar.TabIndex = 16
+        Me.Btncancelar.Text = "Cancelar"
+        Me.Btncancelar.UseVisualStyleBackColor = True
+        '
+        'Btnseleccionar
+        '
+        Me.Btnseleccionar.Location = New System.Drawing.Point(457, 489)
+        Me.Btnseleccionar.Name = "Btnseleccionar"
+        Me.Btnseleccionar.Size = New System.Drawing.Size(100, 39)
+        Me.Btnseleccionar.TabIndex = 15
+        Me.Btnseleccionar.Text = "Seleccionar"
+        Me.Btnseleccionar.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(39, 146)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(723, 325)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'Btnbuscar
+        '
+        Me.Btnbuscar.Location = New System.Drawing.Point(568, 75)
+        Me.Btnbuscar.Name = "Btnbuscar"
+        Me.Btnbuscar.Size = New System.Drawing.Size(100, 39)
+        Me.Btnbuscar.TabIndex = 13
+        Me.Btnbuscar.Text = "Buscar"
+        Me.Btnbuscar.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(57, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "Origen"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(301, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 21
+        Me.Label2.Text = "Fecha"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(304, 41)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 19
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(57, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Tipo de transporte"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(60, 40)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(206, 21)
+        Me.ComboBox1.TabIndex = 17
+        '
+        'Cbxorigen
+        '
+        Me.Cbxorigen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Cbxorigen.FormattingEnabled = True
+        Me.Cbxorigen.Location = New System.Drawing.Point(60, 91)
+        Me.Cbxorigen.Name = "Cbxorigen"
+        Me.Cbxorigen.Size = New System.Drawing.Size(206, 21)
+        Me.Cbxorigen.TabIndex = 23
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(301, 75)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "Destino"
+        '
+        'Cbxdestino
+        '
+        Me.Cbxdestino.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Cbxdestino.FormattingEnabled = True
+        Me.Cbxdestino.Location = New System.Drawing.Point(298, 91)
+        Me.Cbxdestino.Name = "Cbxdestino"
+        Me.Cbxdestino.Size = New System.Drawing.Size(206, 21)
+        Me.Cbxdestino.TabIndex = 25
+        '
+        'Reserva_Buscar_Viaje
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(801, 551)
+        Me.Controls.Add(Me.Cbxdestino)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Cbxorigen)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Btncancelar)
+        Me.Controls.Add(Me.Btnseleccionar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Btnbuscar)
+        Me.Name = "Reserva_Buscar_Viaje"
+        Me.Text = "Reserva_Buscar_Viaje"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Btncancelar As System.Windows.Forms.Button
+    Friend WithEvents Btnseleccionar As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Btnbuscar As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Cbxorigen As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Cbxdestino As System.Windows.Forms.ComboBox
+End Class
---
+++ Diploma-Wolczok/PrototipoMundoTravel/Reserva_Buscar_Viaje.vb
@@ -0,0 +1,66 @@
+﻿Imports BLL
+Imports EE
+
+Public Class Reserva_Buscar_Viaje
+
+    Private Sub Reserva_Buscar_Viaje_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cargarCombos()
+        llenarcombosdestino()
+        llenarcombosorigen()
+
+    End Sub
+
+
+    Private Sub cargarCombos()
+
+        Dim oListatipotrans As New List(Of BE_TipoTransporte)
+        Dim blltipotrans As New BLL_TipoTransporte
+        oListatipotrans = blltipotrans.consultarTipoTransportes
+
+        For Each mitrans As BE_TipoTransporte In oListatipotrans
+            Me.ComboBox1.Items.Add(mitrans)
+
+        Next
+    End Sub
+
+    Private Sub llenarcombosdestino()
+        Dim oListaDestinos As New List(Of BE_Destino)
+        Dim bllDestino As New BLL_Destino
+        oListaDestinos = bllDestino.consultarDestinos
+        For Each miDest As BE_Destino In oListaDestinos
+            Me.Cbxdestino.Items.Add(miDest)
+            Cbxdestino.DisplayMember = "NombreCompleto"
+        Next
+    End Sub
+
+    Private Sub llenarcombosorigen()
+        Dim oListaDestinos As New List(Of BE_Destino)
+        Dim bllDestino As New BLL_Destino
+        oListaDestinos = bllDestino.consultarDestinos
+        For Each miDest As BE_Destino In oListaDestinos
+            Me.Cbxorigen.Items.Add(miDest)
+            Cbxorigen.DisplayMember = "NombreCompleto"
+        Next
+    End Sub
+
+
+
+
+    Private Sub Btnbuscar_Click(sender As Object, e As EventArgs) Handles Btnbuscar.Click
+        'Try
+        '    Dim oListatipotrans As New List(Of BE_TipoTransporte)
+        '    Dim bllTipotransp As New BLL_TipoTransporte
+        '    oListatipotrans = bllTipotransp.consultarTipotransporte(DirectCast(Me.ComboBox1.SelectedItem, EE.BE_TipoTransporte), Cbxdestino, BE_Destino
+        '    DataGridView1.DataSource = Nothing
+        '    DataGridView1.DataSource = oListatipotrans
+        '    DataGridView1.ReadOnly = True
+        '    DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+
+
+        'Catch ex As Exception
+
+        'End Try
+
+
+    End Sub
+End Class
