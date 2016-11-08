6d016dcc7a25de56d2062b581aafb5626cd62781
mwolczok <matias.wolczok@gmail.com>
2016-11-08 00:17:12 -0300

    PAgos

--- Diploma-Wolczok/PrototipoMundoTravel/Visualizar_Pagos.vb
+++ Diploma-Wolczok/PrototipoMundoTravel/Visualizar_Pagos.vb
@@ -21,8 +21,12 @@ Public Class Visualizar_Pagos
     Private Sub TextBox9_TextChanged(sender As Object, e As EventArgs) Handles TextBox9.TextChanged
         Try
             If TextBox9.Text.Length >= 3 Then
+                Dim oPasajero As New BE_Pasajero
+                Dim oReservaviaje As New BE_ReservaViaje
                 Dim oPagoviaje As New BE_PagoViaje
-                oPagoviaje.ID_Reservaviaje.Pasajero.DNI = Me.TextBox9.Text
+                oPasajero.DNI = Me.TextBox9.Text
+                oReservaviaje.Pasajero = oPasajero
+                oReservaviaje.
                 Dim oListapagoviaje As New List(Of BE_PagoViaje)
                 Dim bllPagoviaje As New BLL_PagoViaje
                 oListapagoviaje = bllPagoviaje.consultarPagosViajes(oPagoviaje)
