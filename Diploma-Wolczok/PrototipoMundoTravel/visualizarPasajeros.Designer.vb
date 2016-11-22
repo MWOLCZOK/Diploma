64f9025197b9f49d2fbdcbd7b74716fdb0cf0e57
SantaT89 <santa.tassara@gmail.com>
2016-11-22 15:10:35 -0300

    12

---
+++ Diploma-Wolczok/PrototipoMundoTravel/visualizarPasajeros.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class visualizarPasajeros
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.dgvBitacora = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvBitacora, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(235, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(405, 417)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(150, 30)
+        Me.btnSalir.TabIndex = 54
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'dgvBitacora
+        '
+        Me.dgvBitacora.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgvBitacora.Location = New System.Drawing.Point(50, 87)
+        Me.dgvBitacora.Name = "dgvBitacora"
+        Me.dgvBitacora.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.dgvBitacora.Size = New System.Drawing.Size(563, 305)
+        Me.dgvBitacora.TabIndex = 53
+        '
+        'visualizarPasajeros
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(672, 477)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.dgvBitacora)
+        Me.Name = "visualizarPasajeros"
+        Me.Text = "visualizarPasajeros"
+        CType(Me.dgvBitacora, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents dgvBitacora As DataGridView
+End Class
---
+++ Diploma-Wolczok/PrototipoMundoTravel/visualizarPasajeros.vb
@@ -0,0 +1,30 @@
+﻿Public Class visualizarPasajeros
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Me.Close()
+    End Sub
+
+    Private Sub visualizarPasajeros_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        iniciar()
+    End Sub
+
+    Public Sub iniciar()
+        Try
+            Dim _bllPasajero As New BLL.BLL_Pasajero
+            Dim listaPasajeros As New List(Of EE.BE_Pasajero)
+            listaPasajeros = _bllPasajero.consultarPasajeros()
+            Me.dgvBitacora.DataSource = listaPasajeros
+            EstablecerGrid()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub EstablecerGrid()
+        Me.dgvBitacora.Columns.Remove("ID_")
+        Me.dgvBitacora.Columns("NombreCompleto").Name = "Column_NombreCompleto"
+        Me.dgvBitacora.Columns("DNI").Name = "Column_DNI"
+        Me.dgvBitacora.Columns("Telefono").Name = "Column_Telefono"
+        Me.dgvBitacora.Columns("CorreoElectronico").Name = "Column_MAIL"
+    End Sub
+
+End Class
