edb6277617bfcee12982c231b0ca5cd5aa88c0fc
mwolczok <matias.wolczok@gmail.com>
2016-11-23 20:57:50 -0300

    asd

--- Diploma-Wolczok/BLL/BLL_Cancelacion.vb
+++ Diploma-Wolczok/BLL/BLL_Cancelacion.vb
@@ -9,7 +9,7 @@ Public Class BLL_Cancelacion
     Dim _dalreservaviaje As New DAL_ReservaViaje
     Dim _dalreservaaloja As New DAL_ReservaAlojamiento
 
-    Public Sub alta(ByVal paramCancelacion As BE_Cancelacion, ByVal paramPago As BE_Pago, ByVal paramReservaviaje As BE_ReservaViaje, ByVal paramReservaAloja As BE_ReservaAlojamiento)
+    Public Sub alta(ByVal paramCancelacion As BE_Cancelacion)
         Try
             '   LA DESCRIPCION VIENE EN EL OBJETO DESDE EL FORMULARIO
 
@@ -20,20 +20,7 @@ Public Class BLL_Cancelacion
 
 
             '   AGREGAR DATOS DE LA CANCELACION
-            _cancelacion.alta(paramCancelacion)
-            _dalpago.actualizarpagocancel(paramCancelacion)
-            Dim oListaReserva As New List(Of BE_ReservaViaje)
-            For Each reservavi In oListaReserva
-                _dalreservaviaje.actualizarreservaviajecancel(reservavi)
-            Next
-            Dim oListaReservaaloja As New List(Of BE_ReservaViaje)
-            For Each reservaal In oListaReservaaloja
-                _dalreservaaloja.actualizarreservaalojacancel(reservaal)
-            Next
-            Dim oListapago As New List(Of BE_Pago)
-            For Each oPago In oListapago
-                _dalpago.actualizarpagocancel(oPago)
-            Next
+
             '_dalreservaaloja.actualizarreservaalojacancel(paramCancelacion)
             '_dalreservaviaje.actualizarreservaviajecancel(paramCancelacion)
 
