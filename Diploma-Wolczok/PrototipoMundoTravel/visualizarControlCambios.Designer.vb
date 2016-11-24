a99ac5812a35dc3d568980bfa6ea0b1e9675d849
SantaT89 <santa.tassara@gmail.com>
2016-11-23 21:29:27 -0300

    ControldeCambios

---
+++ Diploma-Wolczok/PrototipoMundoTravel/visualizarControlCambios.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class visualizarControlCambios
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(visualizarControlCambios))
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.dgvBitacora = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvBitacora, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.BackColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(235, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.btnBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBuscar.Location = New System.Drawing.Point(529, 40)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(150, 30)
+        Me.btnBuscar.TabIndex = 56
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = False
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(235, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(311, 455)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(150, 30)
+        Me.btnSalir.TabIndex = 55
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(21, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(143, 15)
+        Me.Label1.TabIndex = 54
+        Me.Label1.Text = "Selección de Cambio"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(24, 46)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(305, 21)
+        Me.ComboBox1.TabIndex = 53
+        '
+        'dgvBitacora
+        '
+        Me.dgvBitacora.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.dgvBitacora.Location = New System.Drawing.Point(24, 81)
+        Me.dgvBitacora.Name = "dgvBitacora"
+        Me.dgvBitacora.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.dgvBitacora.Size = New System.Drawing.Size(738, 348)
+        Me.dgvBitacora.TabIndex = 52
+        '
+        'visualizarControlCambios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(153, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(791, 519)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.dgvBitacora)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "visualizarControlCambios"
+        Me.Text = "visualizarControlCambios"
+        CType(Me.dgvBitacora, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnBuscar As System.Windows.Forms.Button
+    Friend WithEvents btnSalir As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents dgvBitacora As System.Windows.Forms.DataGridView
+End Class
---
+++ Diploma-Wolczok/PrototipoMundoTravel/visualizarControlCambios.vb
@@ -0,0 +1,68 @@
+﻿Imports BLL
+Imports EE
+
+Public Class visualizarControlCambios
+    Implements BLL.BLL_Iobservador
+
+    Private Sub visualizarControlCambios_HelpRequested(sender As Object, hlpevent As HelpEventArgs) Handles Me.HelpRequested
+        Try
+            Dim RutaDeaplicacion As String = Application.StartupPath & "\Ayuda-MundoTravel.chm"
+            Help.ShowHelp(ParentForm, RutaDeaplicacion, HelpNavigator.KeywordIndex, "")
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Public Sub iniciar()
+        Dim bllBitacora As New BLL_Bitacora
+        Dim listaControl As New List(Of EE.BE_Usuario_Control_Cambios)
+        listaControl = bllBitacora.ConsultarGestorCambios()
+        Me.dgvBitacora.DataSource = listaControl
+        For Each miDestino As EE.BE_Usuario_Control_Cambios In listaControl
+            If comprobarsiesta(miDestino) = False Then
+                Me.ComboBox1.Items.Add(miDestino)
+            End If
+
+        Next
+    End Sub
+
+    Private Function comprobarsiesta(ByVal paramCambio As BE_Usuario_Control_Cambios) As Boolean
+        For Each item As Object In ComboBox1.Items
+            If DirectCast(item, EE.BE_Usuario_Control_Cambios).ID_Cambio = paramCambio.ID_Cambio Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+
+
+
+    Private Sub visualizarControlCambios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        iniciar()
+        SessionBLL.SesionActual.agregarForm(Me)
+        SessionBLL.SesionActual.notificarCambiodeIdioma()
+    End Sub
+
+    Public Sub actualizarIdioma(ParamObservador As BLL.BLL_SesionObservada) Implements BLL.BLL_Iobservador.actualizarIdioma
+        Dim MiTraductor As New ControladorTraductor
+        MiTraductor.TraducirForm(SessionBLL.SesionActual.ListaForm.Item(SessionBLL.SesionActual.ListaForm.IndexOf(Me)))
+
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        If Not IsNothing(ComboBox1.SelectedItem) Then
+            Dim bllBitacora As New BLL_Bitacora
+            Dim listaControl As New List(Of EE.BE_Usuario_Control_Cambios)
+            Dim beCambio As New BE_Usuario_Control_Cambios
+            beCambio = DirectCast(ComboBox1.SelectedItem, BE_Usuario_Control_Cambios)
+            listaControl = bllBitacora.ConsultarGestorCambios(beCambio)
+            dgvBitacora.DataSource = Nothing
+            dgvBitacora.DataSource = listaControl
+        End If
+
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Me.Close()
+    End Sub
+End Class
